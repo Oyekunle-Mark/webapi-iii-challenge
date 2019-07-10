b4d2b58f52804a482e52304997a4bda80a977d59
oyekunle-mark <oyekunlemac@gmail.com>
2019-07-10 16:41:31 +0100

    Refactored the post router

--- posts/postRouter.js
+++ posts/postRouter.js
@@ -1,52 +1,19 @@
 const express = require('express');
 
+const handlers = require('./handlers');
 const postValidator = require('../middlewares/postValidators');
 
 const router = express.Router();
-const Posts = require('./postDb');
 
-router.get('/', (req, res) => {
-  Posts.get()
-    .then(posts => res.status(200).json(posts))
-    .catch(err => res.status(500).json(err));
-});
-
-router.post('/', postValidator.validatePost, (req, res) => {
-  const { text, user_id } = req.body;
-
-  Posts.insert({ text, user_id })
-    .then(post => res.status(201).json(post))
-    .catch(err => res.status(500).json(err));
-});
-
-router.get('/:id', postValidator.validatePostId, (req, res) => {
-  const { id } = req.params;
-
-  Posts.getById(id)
-    .then(post => res.status(200).json(post))
-    .catch(err => res.status(500).json(err));
-});
-
-router.delete('/:id', postValidator.validatePostId, (req, res) => {
-  const { id } = req.params;
-
-  Posts.remove(id)
-    .then(post => res.status(200).json(`${post} Record deleted`))
-    .catch(err => res.status(500).json(err));
-});
-
+router.get('/', handlers.getPosts);
+router.post('/', postValidator.validatePost, handlers.createPost);
+router.get('/:id', postValidator.validatePostId, handlers.getPostById);
+router.delete('/:id', postValidator.validatePostId, handlers.deletePosts);
 router.put(
   '/:id',
   postValidator.validatePostId,
   postValidator.validatePost,
-  (req, res) => {
-    const { id } = req.params;
-    const { text } = req.body;
-
-    Posts.update(id, { text })
-      .then(post => res.status(200).json(`${post} Record updated`))
-      .catch(err => res.status(500).json(err));
-  },
+  handlers.editPost,
 );
 
 module.exports = router;
--- users/userRouter.js
+++ users/userRouter.js
@@ -6,15 +6,10 @@ const userValidator = require('../middlewares/userValidators');
 const router = express.Router();
 
 router.post('/', userValidator.validateUser, handlers.createUser);
-
 router.get('/', handlers.getUsers);
-
 router.get('/:id', userValidator.validateUserId, handlers.getUserById);
-
 router.get('/:id/posts', userValidator.validateUserId, handlers.getUserPost);
-
 router.delete('/:id', userValidator.validateUserId, handlers.deleteUser);
-
 router.put(
   '/:id',
   userValidator.validateUser,
