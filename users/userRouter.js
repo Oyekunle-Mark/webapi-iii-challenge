8948452a3567945ae2dd774b07f557773e208dc3
oyekunle-mark <oyekunlemac@gmail.com>
2019-07-10 16:21:12 +0100

    Refactored the user router

--- users/userRouter.js
+++ users/userRouter.js
@@ -1,60 +1,25 @@
 const express = require('express');
 
+const handlers = require('./handlers');
 const userValidator = require('../middlewares/userValidators');
 
 const router = express.Router();
-const Users = require('./userDb');
 
-router.post('/', userValidator.validateUser, (req, res) => {
-  const { name } = req.body;
+router.post('/', userValidator.validateUser, handlers.createUser);
 
-  Users.insert({ name })
-    .then(user => res.status(201).json(user))
-    .catch(err => res.status(500).json(err));
-});
+router.get('/', handlers.getUsers);
 
-router.get('/', (req, res) => {
-  Users.get()
-    .then(users => res.status(200).json(users))
-    .catch(err => res.status(500).json(err));
-});
+router.get('/:id', userValidator.validateUserId, handlers.getUserById);
 
-router.get('/:id', userValidator.validateUserId, (req, res) => {
-  const { id } = req.params;
+router.get('/:id/posts', userValidator.validateUserId, handlers.getUserPost);
 
-  Users.getById(id)
-    .then(user => res.status(200).json(user))
-    .catch(err => res.status(500).json(err));
-});
-
-router.get('/:id/posts', userValidator.validateUserId, (req, res) => {
-  const { id } = req.params;
-
-  Users.getUserPosts(id)
-    .then(posts => res.status(200).json(posts))
-    .catch(err => res.status(500).json(err));
-});
-
-router.delete('/:id', userValidator.validateUserId, (req, res) => {
-  const { id } = req.params;
-
-  Users.remove(id)
-    .then(user => res.status(200).json(`${user} user deleted.`))
-    .catch(err => res.status(500).json(err));
-});
+router.delete('/:id', userValidator.validateUserId, handlers.deleteUser);
 
 router.put(
   '/:id',
   userValidator.validateUser,
   userValidator.validateUserId,
-  (req, res) => {
-    const { id } = req.params;
-    const { name } = req.body;
-
-    Users.update(id, { name })
-      .then(user => res.status(200).json(`${user} user successfully updated.`))
-      .catch(err => res.status(500).json(err));
-  },
+  handlers.editUser,
 );
 
 module.exports = router;
